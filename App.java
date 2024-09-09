25c168c49aef552e80c6f72e1803bad7d25762f2
siyeon02 <chrysapark@cau.ac.kr>
2024-09-09 16:31:32 +0900

    결과값 컬렉션 저장 수정

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 package calculator_2;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -7,21 +8,44 @@ public class App {
         Calculator calc = new Calculator();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("첫번째 양수를 입력하시오 : ");
-        int a = sc.nextInt();
-        calc.setFirstOperand(a);
+        while(true){
 
-        sc.nextLine();
+            System.out.println("종료는 exit 입력/종료 원치 않으면 아무키나 입력: ");
+            String text = sc.next();
+            if(text.equals("exit")){
+                break;
+            }
 
-        System.out.println("연산자를 입력하시오 : ");
-        String operator = sc.nextLine();
-        calc.setOperator(operator);
+            System.out.println("첫번째 양수를 입력하시오 : ");
+            int a = sc.nextInt();
+            calc.setFirstOperand(a);
 
-        System.out.println("두번째 양수를 입력하시오 : ");
-        int b = sc.nextInt();
-        calc.setSecondOperand(b);
+            sc.nextLine();
 
-        System.out.println("결과 : " + calc.operation(a,b,operator));
+            System.out.println("연산자를 입력하시오 : ");
+            String operator = sc.next();
+            calc.setOperator(operator);
+
+            System.out.println("두번째 양수를 입력하시오 : ");
+            int b = sc.nextInt();
+            calc.setSecondOperand(b);
+
+            System.out.println("결과 : " + calc.operation(a,b,operator));
+
+            System.out.println("첫번째 결과 값을 삭제하시겠습니까? (y/n): ");
+            String answer = sc.next();
+
+            if(Objects.equals(answer, "y")){
+                calc.deleteFirst();
+                System.out.println("첫번째 결과 값이 삭제되었습니다.");
+
+            }
+            else{
+                continue;
+            }
+
+
+        }
 
 
     }
--- Calculator.java
+++ Calculator.java
@@ -1,51 +1,56 @@
 package calculator_2;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
     public int firstOperand;
     public int secondOperand;
     public String operator;
+    ArrayList<Integer> results = new ArrayList<>();
+    int result = 0;
 
     public Calculator() {}
      public int operation(int firstOperand, int secondOperand, String operator) {
-         while (true) {
-             Scanner sc = new Scanner(System.in);
 
-             System.out.println("종료는 exit 입력");
-             String text = sc.nextLine();
-             if(text.equals("exit")){
-                 break;
-             }
+
+         //while (true) {
+             Scanner sc = new Scanner(System.in);
 
              switch (operator) {
                  case "+":
-                     //System.out.println("결과 : ");
-                     return firstOperand + secondOperand;
-                 //break;
+                     result = firstOperand + secondOperand;
+                     results.add(result);
+                     return results.get(results.size()-1);
+
                  case "-":
-                     //System.out.println("결과 : ");
-                     return firstOperand - secondOperand;
-                 //break;
+                     result = firstOperand - secondOperand;
+                     results.add(result);
+                     return results.get(results.size()-1);
+
                  case "*":
-                     //System.out.println("결과 : ");
-                     return firstOperand * secondOperand;
-                 //break;
+                     result = firstOperand * secondOperand;
+                     results.add(result);
+                     return results.get(results.size()-1);
+
                  case "/":
                      if(secondOperand == 0){
                          System.out.println("나눗셈 연산에서 두번째 정수에 0이 입력될 수 없습니다!");
                          break;
                      }
                      else{
-                         //System.out.println("결과 : ");
-                         return firstOperand / secondOperand;
+                         result = firstOperand / secondOperand;
+                         results.add(result);
+                         return results.get(results.size()-1);
 
                      }
+                 default:
+                     System.out.println("다시 입력해주세요 : ");
 
              }
-
-         }
-
+        // }
          return 0;
      }
 
@@ -64,6 +69,12 @@ public class Calculator {
     public String setOperator(String operator) {
         this.operator = operator;
         return this.operator;
+    }
+
+    public String deleteFirst(){
+        results.remove(0);
+        System.out.println(results.toString());
+        return this.results.toString();
     }
 
 
